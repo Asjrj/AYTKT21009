f34d9a0cf3b019b686516bce5ae13aaeb734842a
Antti Penttinen <asjrj200@gmail.com>
2018-09-24 11:23:38 +0300

    Tehtävä 2.1

---
+++ osa2/alkeet/src/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+
+const Kurssi = (props) => {
+    return (
+        <div>
+            <Otsikko kurssi={props.kurssi} />
+            <Sisalto kurssi={props.kurssi} />
+        </div>
+    )
+}
+
+const Otsikko = (props) => {
+    return (
+        <h1>{props.kurssi.nimi}</h1>
+    )
+}
+
+const Sisalto = (props) => {
+    const rivit = () => props.kurssi.osat.map((osa) => <Osa key={osa.id} osa={osa} />)
+    return (
+        <div>
+            {rivit()}
+        </div>
+    )
+}
+
+const Osa = (props) => {
+    return (
+        <p>{props.osa.nimi} {props.osa.tehtavia}</p>
+    )
+}
+
+const App = () => {
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+            {
+                nimi: 'Reactin perusteet',
+                tehtavia: 10,
+                id: 1
+            },
+            {
+                nimi: 'Tiedonvälitys propseilla',
+                tehtavia: 7,
+                id: 2
+            },
+            {
+                nimi: 'Komponenttien tila',
+                tehtavia: 14,
+                id: 3
+            },
+            {
+                nimi: 'Uusi kurssi',
+                tehtavia: 11,
+                id: 4
+            }
+        ]
+    }
+
+    return (
+        <div>
+            <Kurssi kurssi={kurssi} />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
