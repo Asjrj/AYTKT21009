c820e622d05fc4852cc3f56c41cdb0cca2df2ede
Antti Penttinen <asjrj200@gmail.com>
2018-09-24 14:56:22 +0300

    Tehtävä 2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -7,9 +7,10 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-123456' }
       ],
-      newName: ''
+      newName: '',
+      newNumber: ''
     }
   }
 
@@ -17,11 +18,18 @@ class App extends React.Component {
     this.setState({ newName: event.target.value })
   }
 
+  handleNumberChange = (event) => {
+    this.setState({ newNumber: event.target.value })
+  }
+
   handleAdd = (event) => {
     event.preventDefault();
     let namesFound = this.state.persons.filter((x) => x.name === this.state.newName);
     if (namesFound.length === 0) {
-      let newPerson = { name: this.state.newName };
+      let newPerson = {
+        name: this.state.newName,
+        number: this.state.newNumber,
+      };
       const newPersons = this.state.persons.concat(newPerson);
       this.setState({ persons: newPersons });
     }
@@ -33,16 +41,19 @@ class App extends React.Component {
         <h2>Puhelinluettelo</h2>
         <form>
           <div>
-            nimi: <input value={this.state.newName} onChange={this.handleNameChange} />
+            <p>nimi: <input value={this.state.newName} onChange={this.handleNameChange} /></p>
+            <p>numero: <input value={this.state.newNumber} onChange={this.handleNumberChange} /></p>
           </div>
           <div>
             <button type="submit" onClick={this.handleAdd}>lisää</button>
           </div>
         </form>
         <h2>Numerot</h2>
-        <ul>
-          {this.state.persons.map((person) => <li key={person.name}>{person.name}</li>)}
-        </ul>
+        <table>
+          <tbody>
+            {this.state.persons.map((person) => <tr key={person.name}><td>{person.name}</td><td>{person.number}</td></tr>)}
+          </tbody>
+        </table>
       </div>
     )
   }
