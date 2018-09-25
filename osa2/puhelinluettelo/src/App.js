6d213cd95844ce5af6bd6c5bb9a2c9d9f0ab3597
Antti Penttinen <asjrj200@gmail.com>
2018-09-25 13:44:51 +0300

    Tehtävä 2.10

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,6 +2,21 @@ import React from 'react';
 import './App.css';
 
 
+const UusiHenkilo = (props) => {
+  return (
+    <form>
+      <div>
+        <h1>Lisää uusi</h1>
+        <p>nimi: <input value={props.tila.newName} onChange={props.handleNameChange} /></p>
+        <p>numero: <input value={props.tila.newNumber} onChange={props.handleNumberChange} /></p>
+      </div>
+      <div>
+        <button type="submit" onClick={props.handleAdd}>lisää</button>
+      </div>
+    </form>
+  )
+}
+
 const Luettelo = (props) => {
   if (props.tila.filteredPersons.length === 0) {
     return (
@@ -40,12 +55,17 @@ class App extends React.Component {
     }
   }
 
-  handleNameChange = (event) => {
-    this.setState({ newName: event.target.value })
+  handleNameChange = () => {
+    const toiminto = (event) => {
+      this.setState({ newName: event.target.value })
+    }
+    return toiminto;
   }
 
-  handleNumberChange = (event) => {
-    this.setState({ newNumber: event.target.value })
+  handleNumberChange = () => {
+    return (event) => {
+      this.setState({ newNumber: event.target.value })
+    }
   }
 
   handleFilterChange = (event) => {
@@ -64,17 +84,19 @@ class App extends React.Component {
     this.setState({ filteredPersons: filteredList });
   }
 
-  handleAdd = (event) => {
-    event.preventDefault();
-    let namesFound = this.state.persons.filter((x) => x.name === this.state.newName);
-    if (namesFound.length === 0) {
-      let newPerson = {
-        name: this.state.newName,
-        number: this.state.newNumber,
-      };
-      let newPersons = this.state.persons.concat(newPerson);
-      this.setState({ persons: newPersons });
-      this.filterPersons(this.state.filterName, newPersons);
+  handleAdd = () => {
+    return (event) => {
+      event.preventDefault();
+      let namesFound = this.state.persons.filter((x) => x.name === this.state.newName);
+      if (namesFound.length === 0) {
+        let newPerson = {
+          name: this.state.newName,
+          number: this.state.newNumber,
+        };
+        let newPersons = this.state.persons.concat(newPerson);
+        this.setState({ persons: newPersons });
+        this.filterPersons(this.state.filterName, newPersons);
+      }
     }
   }
 
@@ -83,16 +105,10 @@ class App extends React.Component {
       <div>
         <h2>Puhelinluettelo</h2>
         <p>rajaa näytettäviä: <input value={this.state.filterName} onChange={this.handleFilterChange} /></p>
-        <form>
-          <div>
-            <h1>Lisää uusi</h1>
-            <p>nimi: <input value={this.state.newName} onChange={this.handleNameChange} /></p>
-            <p>numero: <input value={this.state.newNumber} onChange={this.handleNumberChange} /></p>
-          </div>
-          <div>
-            <button type="submit" onClick={this.handleAdd}>lisää</button>
-          </div>
-        </form>
+        <UusiHenkilo tila={this.state}
+          handleNameChange={this.handleNameChange()}
+          handleNumberChange={this.handleNumberChange()}
+          handleAdd={this.handleAdd()} />
         <h2>Numerot</h2>
         <Luettelo tila={this.state} />
       </div>
