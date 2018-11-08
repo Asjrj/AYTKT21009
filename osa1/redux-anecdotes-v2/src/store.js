cccaa0b6e1e2e41fb2ee40ade6d0ef0dab83dcec
Antti Penttinen <asjrj200@gmail.com>
2018-11-08 10:39:57 +0200

    Tehtävä 6.10

--- osa1/redux-anecdotes-v2/src/store.js
+++ osa1/redux-anecdotes-v2/src/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers } from 'redux'
 import filterReducer from './reducers/filterReducer'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
+import anecdoteService from './services/anecdotes'
 
 const reducer = combineReducers({
   filter: filterReducer,
@@ -10,5 +11,11 @@ const reducer = combineReducers({
 })
 
 const store = createStore(reducer)
+
+anecdoteService.getAll().then(anecdotes =>
+  anecdotes.forEach(anecdote => {
+    store.dispatch({ type: 'NEW_ANECDOTE', data: anecdote })
+  })
+)
 
 export default store
